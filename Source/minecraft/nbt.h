50e21f5879ffe10a0e777ac1ee8bdb0a70484211
Derek <derekforgione@gmail.com>
2021-07-20 17:26:46 -0700

    Forgot to commit

--- Source/minecraft/nbt.h
+++ Source/minecraft/nbt.h
@@ -1,9 +1,44 @@
 ﻿#pragma once
 
+#include<iostream>>
+
 //╔════════════════════════════════════════════════════════╗
 //║ Includes                                               ║
 //╚════════════════════════════════════════════════════════╝
 #pragma region [Includes]
+
+// Things that a sub-class of NbtTag needs to be able to do:
+// - Write to stream
+// - Read from stream
+// - Convert to bytes
+// - Convert from bytes
+
+class NbtTag
+{
+	private:
+		unsigned char type_code = 0;
+	public:
+		union
+		{
+			unsigned int _uint;
+			std::string String;
+		};
+
+		NbtTag& operator=(const std::string& str)
+		{
+			this->String = str;
+			this->type_code = 2;
+			return *this;
+		}
+
+		NbtTag& operator=(unsigned int value)
+		{
+			this->_uint = value;
+			this->type_code = 1;
+			return *this;
+		}
+
+};
 
 
 #pragma endregion [Includes]
