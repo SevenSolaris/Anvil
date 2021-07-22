5e77852dbb1a53d8e93c8ae2dbb8681df4974e3f
Derek <derekforgione@gmail.com>
2021-07-22 11:19:40 -0700

    Moved; Also added imgui to the includes.

--- Source/precompile.cpp
+++ Source/precompile.cpp
@@ -13,6 +13,10 @@ precompile in order to make compilation times faster.
 #define STB_IMAGE_IMPLEMENTATION
 #include<stb/stb_image.h>
 
+#include<imgui.h>
+#include<imgui_impl_glfw.h>
+#include<imgui_impl_opengl3.h>
+
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
